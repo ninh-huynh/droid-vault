6421411d66913f319ff5b38a0c2b8112a6589ad9
Ninh Huynh <kimninh1610@gmail.com>
2025-03-16 23:02:26 +0700

    setup ci cd

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4",
+    pageTitle: "👨‍💻 Droid Vault",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
